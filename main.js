ffb29bff1f424d90eb1c4db7d17f38433a20bcc9
Glados <devilsparta@live.cn>
2018-04-05 23:45:47 +0800

    add:Send Something Interval

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 var MsgSender = new WebSocket("ws://127.0.0.1:1000");
-
-MsgSender.addEventListener("open", function(ev){
+var IsOpen = false;
+MsgSender.addEventListener("open", function (ev) {
+	IsOpen = true;
 	console.log("open", ev);
 })
 
-MsgSender.addEventListener("message", function(ev){
-	console.log("message",ev);
+MsgSender.addEventListener("message", function (ev) {
+	console.log("message", ev);
 })
 
-MsgSender.addEventListener("error", function(ev){
-	console.log("error",ev);
+MsgSender.addEventListener("error", function (ev) {
+	console.log("error", ev);
 })
 
-MsgSender.addEventListener("close", function(ev){
-	console.log("close",ev);
+MsgSender.addEventListener("close", function (ev) {
+	console.log("close", ev);
 })
+
+setInterval(function () {
+	if (!IsOpen) {
+		return false;
+	}
+	var jsonData = JSON.stringify({ a: 100 });
+	MsgSender.send(jsonData);
+
+}, 1000);
