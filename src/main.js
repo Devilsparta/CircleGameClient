9f79ebeb5896557c2af694fe8f0ae9a26b6054fe
Glados <devilsparta@live.cn>
2018-04-06 17:34:27 +0800

    add something

---
+++ src/main.js
@@ -0,0 +1,31 @@
+var IsOpen = false;
+var MsgSender = null;
+
+function Login(userName) {
+	MsgSender = new WebSocket("ws://127.0.0.1:1000");
+	MsgSender.addEventListener("open", function (ev) {
+		IsOpen = true;
+		console.log("open", ev);
+	})
+
+	MsgSender.addEventListener("message", function (ev) {
+		console.log("message", ev);
+	})
+
+	MsgSender.addEventListener("error", function (ev) {
+		console.log("error", ev);
+	})
+
+	MsgSender.addEventListener("close", function (ev) {
+		console.log("close", ev);
+	})
+}
+
+setInterval(function () {
+	if (!IsOpen) {
+		return false;
+	}
+	var jsonData = JSON.stringify({ a: 100 });
+	MsgSender.send(jsonData);
+}, 1000);
+
